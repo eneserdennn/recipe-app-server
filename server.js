5dfc8484525e23f524d2381ec08bcc71078a9275
Enes Erden <admin@eneserden.com>
2022-06-04 21:11:48 +0300

    Deploy Fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/recipes", require("./routes/recipeRoutes"));
+app.use("/", require("./routes/recipeRoutes"));
 
 app.use(errorHandler);
 
